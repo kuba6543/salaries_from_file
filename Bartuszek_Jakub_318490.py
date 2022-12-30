a5be71dbc0e20e573e79400685cf358130404969
Jakub Bartuszek <01169002@pw.edu.pl>
2022-12-30 17:15:13 +0100

    Project still not working, but made code more readable

--- Bartuszek_Jakub_318490.py
+++ Bartuszek_Jakub_318490.py
@@ -7,33 +7,46 @@ def read_from_file(filename):
         temp = line.split(";")
         temp.pop(len(temp)-1) # Remove /n from the table
         struct.append(temp)
+    file.close()
     return struct
 
 def calculations(struct):
+    i=0
     salary=[]
-    for i in struct:
-        if(struct[i][1]=="UoP"):
-            salary[i] = float(struct[i][2]) + (float(struct[i][3]) * int(struct[i][4]))
-        elif(struct[i][1]=="UZ"):
-            salary[i] = float(struct[i][2]) * int(struct[i][3])
-        elif(struct[i][1]=="UoD"):
+    for each in struct:
+        if(struct[i][1]=="UoD"):
             if(struct[i][3]=="YES"):
-                salary[i] = int(struct[i][2])
+                salary[i] = float(struct[i][2])
             else:
                 salary[i] = 0
+        elif(struct[i][1]=="UZ"):
+            bid=float(struct[i][2])
+            hours=float(struct[i][3])
+            salary[i] = bid * hours
+        elif(struct[i][1]=="UoP"):
+            basic_salary=float(struct[i][2])
+            overtime_hours=float(struct[i][3])
+            overtime_bid=float(struct[i][4])
+            salary[i] = basic_salary + overtime_hours * overtime_bid
+        i=i+1
+        
     return salary
 
-def write_file(struct, salary): 
+def write_file(struct, salary, output_file): 
     output=[]
     sum_of_salaries=0
     for i in salary:
         sum_of_salaries += salary[i]
     output[0]=["Sum of salaries", sum_of_salaries]
     i=0
+    if(output_file):    file=open(output_file, mode="w", encoding="utf8")
+    else:               file=open("a.out", mode="w",encoding="utf8")
     for each in struct:
+        file.writelines(output[i])
         output[i+1]=[struct[i][0],salary[i]]
-    print(output)
+        i=i+1
+    file.close()
 
 struct = read_from_file(sys.argv[1])
 salary = calculations(struct)
-write_file(struct, salary)
+write_file(struct, salary, sys.argv[2])
--- README.md
+++ README.md
@@ -4,3 +4,23 @@
 
 Program will read data from file, count and show salaries of workers mentioned in file.
 
+## Variables in programme:
+
+* struct - struct of data, read from file
+* salary - calculated salaries, ready to be written
+
+## Functions in programme:
+
+* read_from_file(filename) - reads a file and puts data into a list named "struct"
+* calcualtions(struct) - calculates salaries from "struct" and puts calculations to list named "salary"
+* write_file(struct, salary, output_file) - uses both lists "struct" and "salary" and writes to output_file if defined (if not defined, the file will be written as "a.out")
+
+## Exemplary input and output:
+
+```
+python Bartuszek_Jakub_318490.py input.txt
+```
+or
+```
+python Bartuszek_Jakub_318490.py input.txt output.txt
+```
