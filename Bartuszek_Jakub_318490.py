5da59973a2df002bcb08a1996fc86b7dbfa6c793
Jakub Bartuszek <01169002@pw.edu.pl>
2023-01-06 00:42:03 +0100

    Added exceptions in case of not proper amount of arguments

--- Bartuszek_Jakub_318490.py
+++ Bartuszek_Jakub_318490.py
@@ -3,44 +3,44 @@ try:
 except:
     raise Exception("Library sys not found, please consider reinstalling Python enviroment")
 
-def read_from_file(filename):
+def read_data_from_a_file(filename):
     file = open(filename,mode="r", encoding='utf8')
-    struct = []
+    database = []
     for line in file:
         temp = line.split(";")
         temp.pop(len(temp)-1) # Remove /n from the table
-        struct.append(temp)
+        database.append(temp)
     file.close()
-    return struct
+    return database
 
-def calculations(struct):
+def calculate_salaries_from_database(database):
     i=0
-    salary=[]
-    for each in struct:
-        if(struct[i][1]=="UoD"):
-            if(struct[i][3]=="YES"):
-                salary.append(float(struct[i][2]))
+    salaries=[]
+    for each in database:
+        if(database[i][1]=="UoD"):
+            if(database[i][3]=="YES"):
+                salaries.append(float(database[i][2]))
             else:
-                salary.append(0)
-        elif(struct[i][1]=="UZ"):
-            bid=float(struct[i][2])
-            hours=float(struct[i][3])
-            salary.append(bid * hours)
-        elif(struct[i][1]=="UoP"):
-            basic_salary=float(struct[i][2])
-            overtime_hours=float(struct[i][3])
-            overtime_bid=float(struct[i][4])
-            salary.append(basic_salary + overtime_hours * overtime_bid)
+                salaries.append(0)
+        elif(database[i][1]=="UZ"):
+            bid=float(database[i][2])
+            hours=float(database[i][3])
+            salaries.append(bid * hours)
+        elif(database[i][1]=="UoP"):
+            basic_salaries=float(database[i][2])
+            overtime_hours=float(database[i][3])
+            overtime_bid=float(database[i][4])
+            salaries.append(basic_salaries + overtime_hours * overtime_bid)
         i=i+1
-    return salary
+    return salaries
 
-def write_file(struct, salary, output_file): 
+def write_data_to_a_file(database, salaries, output_file): 
     output=[]
-    output.append(["Sum of salaries", sum(salary)])
+    output.append(["Sum of salaries", sum(salaries)])
     i=0
     file=open(output_file, mode="w", encoding="utf8")
-    for each in struct:
-        output.append([struct[i][0],salary[i]])
+    for each in database:
+        output.append([database[i][0],salaries[i]])
         i=i+1
     i=0
     for each in output:
@@ -48,7 +48,16 @@ def write_file(struct, salary, output_file):
         file.writelines("\n")
         i=i+1
     file.close()
+
 if __name__ == "__main__":
-    struct = read_from_file(sys.argv[1])
-    salary = calculations(struct)
-    write_file(struct, salary, sys.argv[2])
+    try:
+        database = read_data_from_a_file(sys.argv[1])
+    except:
+        sys.exit("Input file not found, please write 'python Bartuszek_Jakub_318490.py <input_file_name> <optional_output_file_name>' in terminal")
+
+    salaries = calculate_salaries_from_database(database)
+    
+    try:
+        write_data_to_a_file(database, salaries, sys.argv[2])
+    except:
+        write_data_to_a_file(database, salaries, "a.out")
--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Program will read data from file, count and show salaries of workers mentioned i
 
 ## Variables in programme:
 
-* struct - struct of data, read from file
-* salary - calculated salaries, ready to be written
+* database - database of data, read from file
+* salaries - calculated salaries, ready to be written
 
 ## Functions in programme:
 
-* read_from_file(filename) - reads a file and puts data into a list named "struct"
-* calcualtions(struct) - calculates salaries from "struct" and puts calculations to list named "salary"
-* write_file(struct, salary, output_file) - uses both lists "struct" and "salary" and writes to output_file (must be defined)
+* read_data_from_a_file(filename) - reads a file and puts data into a list named "database"
+* calculate_salaries_from_database(database) - calculates salaries from "database" and puts calculations to list named "salaries"
+* write_data_to_a_file(database, salaries, output_file) - uses both lists "database" and "salaries" and writes to output_file (may be defined, if not, the output data is saved to a.out file in the same directory)
   
 ## Exemplary input and output:
 input.txt used from main repository folder
@@ -23,6 +23,17 @@ python Bartuszek_Jakub_318490.py input.txt output.txt
 ```
 
 output.txt
+```
+['Sum of salaries', 20475.4]
+['Łukasz Stanisławowski', 7300.0]
+['Marcin Osadowski', 8975.4]
+['Aleksander Jabłonowski', 4200.0]
+
+```
+python Bartuszek_Jakub_318490.py input.txt
+```
+
+a.out
 ```
 ['Sum of salaries', 20475.4]
 ['Łukasz Stanisławowski', 7300.0]
